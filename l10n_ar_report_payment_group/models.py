9fa2887989f434112901d15be035182115b8d5eb
morrillo <morrillo@umich.edu>
2020-02-10 20:38:19 +0000

    Cambios al reporte de recibos

--- l10n_ar_report_payment_group/models.py
+++ l10n_ar_report_payment_group/models.py
@@ -4,7 +4,6 @@ from odoo.exceptions import ValidationError
 class AccountPaymentGroup(models.Model):
     _inherit = "account.payment.group"
 
-    @api.multi
     def payment_print(self):
         report = self.env['ir.actions.report']._get_report_from_name('l10n_ar_report_payment_group.report_payment_group')
         return report.report_action(docids=self)
