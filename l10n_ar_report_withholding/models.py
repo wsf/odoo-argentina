4f8bbb9a157866f13cfc240b37397569e87ff947
morrillo <morrillo@umich.edu>
2020-03-30 22:36:29 +0000

    Se agrego boton para imprimir retenciones

--- l10n_ar_report_withholding/models.py
+++ l10n_ar_report_withholding/models.py
@@ -1,21 +1,26 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
 import logging
 import datetime
 from datetime import datetime, timedelta, date
 
 class AccountPayment(models.Model):
-	_inherit = 'account.paymentt'
+	_inherit = 'account.payment'
 
-"""
-	def _compute_price_subtotal_vat(self):
-		for line in self:
-                    if line.tax_ids:
-                        for tax_id in line.tax_ids:
-                            if tax_id.tax_group_id.tax_type == 'vat':
-                                line.price_subtotal_vat = line.price_subtotal * ( 1 + tax_id.amount / 100 )
+	def btn_print_withholding(self):
+            self.ensure_one()
+            return self.env.ref('l10n_ar_report_withholding.account_payment_withholdings').report_action(self)
 
-	price_subtotal_vat = fields.Float('price_subtotal_vat',compute=_compute_price_subtotal_vat)
-"""
+	def _compute_print_withholding(self):
+            for rec in self:
+                if rec.state == 'posted':
+                    if rec.tax_withholding_id:
+                        rec.print_withholding = True
+                    else:
+                        rec.print_withholding = False
+                else:
+                    rec.print_withholding = False
 
+	print_withholding = fields.Boolean('print_withholding',compute=_compute_print_withholding)
