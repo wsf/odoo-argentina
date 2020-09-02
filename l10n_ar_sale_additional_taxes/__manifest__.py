f78c08c1ff49dba84cf9fe400024db0d2fc95131
Ignacio Buioli <ibuioli@gmail.com>
2020-09-02 02:01:54 -0300

    fix: add depend account_check

--- l10n_ar_sale_additional_taxes/__manifest__.py
+++ l10n_ar_sale_additional_taxes/__manifest__.py
@@ -5,7 +5,7 @@
     'version': '11.0.1.0.0',
     'category': 'Accounting',
     'summary': "Impuestos Adicionales para Ventas - Argentina",
-    'depends': ['base','account','l10n_ar','account_move_tax'],
+    'depends': ['base','account','l10n_ar','account_move_tax','account_check'],
     "data": [
 	"account_view.xml",
         "security/ir.model.access.csv"
