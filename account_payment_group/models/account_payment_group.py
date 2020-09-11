f0750eb8596efcf508101d08f73a91c80c16feaf
Gustavo Orrillo <gustavo.orrillo@moldeointeractive.com.ar>
2020-09-11 10:43:34 -0300

    Agrega importacion de funcion UserError

--- account_payment_group/models/account_payment_group.py
+++ account_payment_group/models/account_payment_group.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import models, api, fields, _
-from odoo.exceptions import ValidationError
+from odoo.exceptions import ValidationError, UserError
 
 import logging
 
