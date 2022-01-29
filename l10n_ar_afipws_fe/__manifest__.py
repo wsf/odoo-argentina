2de60b90cad26edea9be061d4e57eaf6bf6fd2ef
morrillo <morrillo@umich.edu>
2022-01-29 11:18:10 +0000

    Se agrego modulo de QR

--- l10n_ar_afipws_fe/__manifest__.py
+++ l10n_ar_afipws_fe/__manifest__.py
@@ -14,23 +14,16 @@
         'l10n_ar',
         'account_move_tax',
         'account_debit_note',
-        'report_xlsx'
-        #'l10n_ar_account',
     ],
     'external_dependencies': {
     },
     'data': [
         'wizard/afip_ws_consult_wizard_view.xml',
-        #'wizard/afip_ws_currency_rate_wizard_view.xml',
-        #'wizard/res_config_settings_view.xml',
         'views/move_view.xml',
-        #'views/l10n_latam_document_type_view.xml',
         'views/account_journal_view.xml',
         'views/product_uom_view.xml',
         'views/res_currency_view.xml',
         'security/ir.model.access.csv',
-        #views/report_invoice.xml',
-        #'views/menuitem.xml',
     ],
     'demo': [
     ],
