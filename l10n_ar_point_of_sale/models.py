cced7900585e728e33be22d91ad0833319639e07
morrillo <morrillo@umich.edu>
2022-01-30 13:13:41 +0000

    Se removio l10n_ar_point_of_sale como submodulo y se lo agrego como modulo

---
+++ l10n_ar_point_of_sale/models.py
@@ -0,0 +1,50 @@
+from odoo import fields, models, _, api
+import json
+import base64
+
+class PosOrder(models.Model):
+    _inherit = 'pos.order'
+
+    to_invoice = fields.Boolean('To invoice', default=True)
+
+
+    @api.model
+    def create(self, values):
+        values['to_invoice'] = True
+        return super(PosOrder, self).create(values)
+
+    def action_pos_order_invoice(self):
+        moves = self.env['account.move']
+
+        for order in self:
+            # Force company for all SUPERUSER_ID action
+            if order.account_move:
+                moves += order.account_move
+                continue
+
+            if not order.partner_id:
+                raise UserError(_('Please provide a partner for the sale.'))
+
+            move_vals = order._prepare_invoice_vals()
+            new_move = order._create_invoice(move_vals)
+
+            order.write({'account_move': new_move.id, 'state': 'invoiced'})
+            new_move.sudo().with_company(order.company_id).action_post()
+            moves += new_move
+            order._apply_invoice_payments()
+
+        if not moves:
+            return {}
+
+        return {
+            'name': _('Customer Invoice'),
+            'view_mode': 'form',
+            'view_id': self.env.ref('account.view_move_form').id,
+            'res_model': 'account.move',
+            'context': "{'move_type':'out_invoice'}",
+            'type': 'ir.actions.act_window',
+            'nodestroy': True,
+            'target': 'current',
+            'res_id': moves and moves.ids[0] or False,
+        }
+
