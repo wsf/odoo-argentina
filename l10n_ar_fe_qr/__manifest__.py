f5b0cfdc81076e3e1c4eabe7800294d695a0a7ac
morrillo <morrillo@umich.edu>
2022-06-22 10:11:50 +0000

    Se removio l10n_ar_report_fe

--- l10n_ar_fe_qr/__manifest__.py
+++ l10n_ar_fe_qr/__manifest__.py
@@ -2,8 +2,8 @@
     'name': 'l10n_ar_fe_qr',
     'version': '1.0',
     'category': 'l10n_ar_fe_qr',
-    'summary': 'Plants and customers management',
-    'depends': ['account','l10n_ar','l10n_ar_afipws_fe','l10n_ar_report_fe'],
+    'summary': 'Agrega simbolo QR a factura',
+    'depends': ['account','l10n_ar','l10n_ar_afipws_fe'],
     'data': [
         'afip_view.xml',
         'report_template.xml'
