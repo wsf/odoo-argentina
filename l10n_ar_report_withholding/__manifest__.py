0aee4322a951edfd2a3e72aceb1f5c7f67deda26
mariogustavoorrillo <gustavo.orrillo@gmail.com>
2023-01-11 11:56:17 +0000

    Se agrego l10n_ar_report_withholding

--- l10n_ar_report_withholding/__manifest__.py
+++ l10n_ar_report_withholding/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'author': "Moldeo Interactive,ADHOC SA,Odoo Community Association (OCA)",
+    'author': "A2 Systems,Moldeo Interactive,ADHOC SA,Odoo Community Association (OCA)",
     'category': 'Localization/Argentina',
     'depends': [
         'l10n_ar_account_withholding','account_payment_group',
@@ -15,5 +15,5 @@
     'demo': [
 
     ],
-    'version': '13.0.1.0.0',
+    'version': '16.0.1.0.0',
 }
