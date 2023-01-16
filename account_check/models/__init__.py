8ee005f3ec9ce218466b113764acb2c6a1f2cede
mariogustavoorrillo <gustavo.orrillo@gmail.com>
2023-01-16 10:48:35 +0000

    Se agrego account_check

---
+++ account_check/models/__init__.py
@@ -0,0 +1,9 @@
+##############################################################################
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
+from . import account_journal
+from . import account_move
+from . import account_checkbook
+from . import account_check
+from . import account_payment
